56551bbd1438fe6393acec2a0d068dc43da9b375
David <pollitoyeye@gmail.com>
2018-04-19 12:17:26 +0200

    Contact endpoint

--- Proyectos/RestAPI/RestAPI/src/main/java/es/skills2018/api/utils/ServerConstants.java
+++ Proyectos/RestAPI/RestAPI/src/main/java/es/skills2018/api/utils/ServerConstants.java
@@ -5,8 +5,8 @@ public class ServerConstants {
 	//Database
 	public static final String DATABASE_URL = "jdbc:mysql://localhost/skills2018";
 	public static final String DATABASE_CONNECTOR = "com.mysql.jdbc.Driver";
-	public static final String DATABASE_USER = "root";
-	public static final String DATABASE_PASSWORD = "";
+	public static final String DATABASE_USER = "skillsuser";
+	public static final String DATABASE_PASSWORD = "patata2944";
 	//Rest Response
 	public static final String REST_RESPONSE_CHARSET = ";charset=UTF-8";
 	//Pagination
